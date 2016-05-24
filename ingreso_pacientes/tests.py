d4fc246fca56cec5126f38300b2d989c5d16c91e
Rene Balda <rabt89@gmail.com>
2016-05-24 15:29:08 -0500

    Anadiendo comentario

--- ingreso_pacientes/tests.py
+++ ingreso_pacientes/tests.py
@@ -7,8 +7,8 @@ from ingreso_pacientes.models import Paciente
 class CrearPaciente(TestCase):
 
     def crear_paciente(self):
-        self.paciente = Paciente.objects.create(nombres='Juan',apellidos='Vargas',cedula='0955555555');
+        self.paciente = Paciente.objects.create(nombres='Juan',apellidos='Vargas',cedula='0955555555')
 
     def test_crear_paciente(self):
         self.crear_paciente()
-        self.assertIsInstance(self.paciente,Paciente,'paciente creado exitosamente')
+        self.assertIsInstance(self.paciente,Consulta,'paciente creado exitosamente')
