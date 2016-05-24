cd8151218315cbf388b4e1d6f512b07433cbb9e3
Rene Balda <rabt89@gmail.com>
2016-05-24 15:47:41 -0500

    blackbox text

--- ingreso_pacientes/tests.py
+++ ingreso_pacientes/tests.py
@@ -1,14 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
 from ingreso_pacientes.models import Paciente
 
 #Creando casos de prueba
 class CrearPaciente(TestCase):
+    def setUp(self):
+        self.cliente = Client()
 
     def crear_paciente(self):
         self.paciente = Paciente.objects.create(nombres='Juan',apellidos='Vargas',cedula='0955555555')
 
     def test_crear_paciente(self):
         self.crear_paciente()
-        self.assertIsInstance(self.paciente,Consulta,'paciente creado exitosamente')
+        self.assertIsInstance(self.paciente,Paciente,'paciente creado exitosamente')
+
+    def test_crear_paciente_view(self):
+        respuesta = self.cliente.post('/pacientes/',{"nombres":"Jhon","apellidos":"Smith","cedula":"9999999999","nro_historia":"111111"})
+        self.assertContains(respuesta,"guardado correctamente")
